8dd0e8ce8187a90a46333bf7220699771fc5e2b6
Harsh <mailharshkhatri@gmail.com>
2020-01-17 18:32:41 +0530

    hard coded image  displaying with sound

--- challenge 5/static/js/script.js
+++ challenge 5/static/js/script.js
@@ -18,8 +18,39 @@ function blackjackhit() {
 } 
 */
 
-document.querySelector('blackjack-hit-button').addEventListener('click',blackjackhit)
-
-function blackjackhit() {
-    alert("CLIAKCED")
+var blackjackgame={
+    'you':{'scorespan':'your-blackjack-result', 'div':'your-box','score':0},
+    'dealer':{'scorespan':'dealer-blackjack-result', 'div':'dealer-box','score':0 }
 }
+
+const YOU = blackjackgame['you']
+const DEALER=blackjackgame['dealer']
+
+const hitsound=new Audio('static/sounds/swish.m4a')
+/*
+var el=document.getElementById('blackjack-hit-button')
+el.addEventListener('click',blackjackhit);
+*/
+document.querySelector('#blackjack-hit-button').addEventListener('click',blackjackhit)
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackdeal)
+function blackjackhit() {
+    showcard(YOU)
+}
+
+function showcard(activeplayer) {
+    let cardimage=document.createElement('img')
+    cardimage.src='static/images/queen.png'
+    document.querySelector('#'+activeplayer['div']).appendChild(cardimage)
+    hitsound.play()
+}
+
+function blackjackdeal() {
+    let yourimages=document.querySelector('#your-box').querySelectorAll('img')
+    let dealerimages=document.querySelector('#dealer-box').querySelectorAll('img')
+
+    console.log(yourimages)
+    for(i=0;i<yourimages.length;i++) {
+    yourimages[i].remove()}
+    for(i=0;i<dealerimages.length;i++) {
+        dealerimages[i].remove()}
+} 
